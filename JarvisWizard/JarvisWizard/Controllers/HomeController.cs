12c63e33e50d12fc3f9e8801050cc1eaa4632b49
Código, Hardware e Cia <codigohardwareecia@hotmail.com>
2026-03-24 13:36:26 -0300

    Código de exemplo de WebAPI para Linux Raspberry Pi

---
+++ JarvisWizard/JarvisWizard/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JarvisWizard.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("status")]
+        public IActionResult GetStatus()
+        {
+            return Ok(new { msg = "Jarvis Vivo!" });
+        }
+    }
+}
