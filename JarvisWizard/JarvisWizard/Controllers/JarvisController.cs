021d06d61c67c3bf0717d442dc1ad19d9ea2fff3
Código, Hardware e Cia <codigohardwareecia@hotmail.com>
2026-03-28 20:03:51 -0300

    Versão com suporte a GPIO

---
+++ JarvisWizard/JarvisWizard/Controllers/JarvisController.cs
@@ -0,0 +1,39 @@
+﻿using JarvisWizard.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JarvisWizard.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class JarvisController : ControllerBase
+    {
+        private readonly GpioRaspberryService _gpioRaspberryService;
+
+        public JarvisController()
+        {
+            _gpioRaspberryService = new GpioRaspberryService();
+        }
+
+        [HttpGet("rele")]
+        public async Task<IActionResult> Rele([FromQuery] string action, int pulse = 1)
+        {
+            try
+            {
+                if (action.Equals("on"))
+                {
+                    _gpioRaspberryService.Rele(true);
+                    return Ok("Relé ligado");
+                }
+
+   
+                _gpioRaspberryService.Rele(false);
+                return Ok("Relé desligado");
+
+            }
+            catch (Exception ex)
+            {
+                return Ok(ex.Message);
+            }
+        }
+    }
+}
---
+++ JarvisWizard/JarvisWizard/Services/GpioRaspberryService.cs
@@ -0,0 +1,42 @@
+﻿using System.Device.Gpio;
+
+namespace JarvisWizard.Services
+{
+    public class GpioRaspberryService
+    {
+        private readonly int _pin = 18;
+        private readonly GpioController _gpioController;
+
+        public GpioRaspberryService() {
+            _gpioController = new GpioController();
+        }
+
+        public void Rele(bool estado)
+        {
+            try
+            {
+                PinStatus(_pin);
+
+                _gpioController.OpenPin(_pin, PinMode.Output);
+                _gpioController.Write(_pin,estado ? PinValue.High : PinValue.Low);
+                _gpioController.ClosePin(_pin);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public void PinStatus(int pin)
+        {
+            if (!_gpioController.IsPinOpen(pin))
+            {
+                _gpioController.OpenPin(pin, PinMode.Output);
+            }
+            else
+            {
+                _gpioController.ClosePin(pin);
+            }
+        }
+    }
+}
